c76682c62b58c5a9fc1d71fb03adeb06a19b06bd
Tromboman22 <samuelpineau22@gmail.com>
2026-02-13 13:50:58 -0500

    change variable types and simplify code

--- Technical_A1/IRSensor.c
+++ Technical_A1/IRSensor.c
@@ -2,6 +2,7 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 
 #define F_CPU 16000000UL  // UL is unsigned, 16Mhz is arduino nano clock rate
@@ -17,9 +18,9 @@
 #define yloLED PB5    // yellow led (13)
 
 //limits and variables
-const float d2 = 42;
-const float d1 = 14;
-int reading;
+const uint8_t d2 = 42;
+const uint8_t d1 = 14;
+uint16_t reading; // to contain 10-bit value from ADCSRA reading macro
 float distance;
 bool edges_indicator = false;
 uint8_t brightness;  // 8 bits unisgned
@@ -30,26 +31,25 @@ uint8_t brightness;  // 8 bits unisgned
 
 // cpu freq is 16Mhz, desired baud rate is 9600, 
 // uart baud register rate (ubrr) is 16Mhz(16bits * 9600) - 1 = 103 
-void init() {
+void uart_init() {
     // set baud rate to same as Serial(9600)
     UBRR0H = 0;  
     UBRR0L = (F_CPU / (16 * baud_val)) - 1;    // ubrr is a 16-bit register, val instde is near 104 (0x0068) so ubbr high is 0
     // Enable uart communication through register B
     UCSR0B = (1 << TXEN0);  
 
-    // Set frame format: 8 data bits, 1 stop bit (parity is disabled by default) for data packets
-    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+    // Set frame format: 8 data bits (Z02, Z01, Z00 are 011 for 8 bits data)
+    UCSR0C = (0 << USCZ02) | (1 << UCSZ01) | (1 << UCSZ00); //format is called 8N1
 
     // set the pins using the atmel layout
-    DDRB |= (1 << mainLED);   // D11 output (PWM LED)
+    DDRB |= (1 << mainLED);  // D11 output (PWM LED)
     DDRB |= (1 << yloLED);   // D13 output (Yellow LED)
-    DDRC &= ~(1 << IRPin);  // A0 input (ADC)
+    DDRC &= ~(1 << IRPin);   // A0 input (ADC)
 
 }
 
 // PWM setup
-void PWM_init(void)
-{
+void PWM_init(void){
     DDRB |= (1 << mainLED); // output
 
     TCCR2A = (1 << COM2A1) | (1 << WGM20) | (1 << WGM21); // Fast PWM
@@ -58,15 +58,13 @@ void PWM_init(void)
 
 
 // ADC control
-void ADC_init(void)
-{
+void ADC_init(void){
     ADMUX = (1 << REFS0); // AVcc reference, ADC0 default
     ADCSRA = (1 << ADEN)  // Enable ADC
            | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // prescaler 128
 }
 
-uint16_t ADC_read(void)
-{
+uint16_t ADC_read(void){  // 10-bit resolution
     ADCSRA |= (1 << ADSC);        // Start conversion
     while (ADCSRA & (1 << ADSC)); // Wait
     return ADC;
@@ -74,14 +72,12 @@ uint16_t ADC_read(void)
 
 
 // Printing with uart (no printf)
-void uart_putchar(char c)
-{
+void uart_putchar(char c){
     while (!(UCSR0A & (1 << UDRE0))); // Wait until buffer empty
     UDR0 = c;
 }
 
-void print_string(const char *str)
-{
+void print_string(const char *str){
     while (*str)
     {
         uart_putchar(*str);
@@ -89,9 +85,8 @@ void print_string(const char *str)
     }
 }
 
-void print_int(int value)
-{
-    char buffer[10];
+void print_int(int value){
+    char buffer[12];
     itoa(value, buffer, 10);  // convert int to string (base 10)
     print_string(buffer);
 }
@@ -99,12 +94,12 @@ void print_int(int value)
 
 int main(void) {
   // setup all in this func
-  init();
+  uart_init();
   ADC_init();
   PWM_init();
 
   while(1){
-    reading = 0;
+    reading = 0; // *** check later ***
     // get multiple data points since the sensor is suceptible to noise
     for(int i = 0; i < ADC_sample_max; ++i){
       reading += ADC_read();
@@ -115,7 +110,7 @@ int main(void) {
     // account for false readings below 20, this uses the logic from SharpIR.cpp in the SharpIR ghithub library at https://github.com/qub1750ul/Arduino_SharpIR/blob/master/src/SharpIR.cpp
     // Sensor is GP2Y0A21YK0F, added a layer of protection in case of a very low reading bug
     if (reading > 20) {
-      distance = 4800.0 / (reading - 20);
+      distance = 4800.0 / (reading - 20); //*** need f for float?? ***
     } else {
       distance = d2 + 1;   // force out-of-bounds high, low reading means farther away as per the data sheet
     }
@@ -132,13 +127,16 @@ int main(void) {
     } 
 
     // brightness 
-    if(distance < d1) brightness = 255;   // edge cases, LED is brightest at brightness = 0
-    else if(distance > d2) brightness = 0;
-    else{     // scale the brightness linearly from 14cm to 42cm
-      brightness = 255 * (1 - (distance - d1)/(d2-d1)); 
-      // if the brightness ever inverts for some reason, add failsafe to make sure brightness never goes past edges, as brightness is uint8_t
-      // ex: brightness = -4 --> becomes brightness = 252
+    if(distance <= d1) {    // account for overflow
+      distance = d1;
+    }else if(distance >= d2){ 
+      distance = d2;
     }
+    // scale the brightness linearly from 14cm to 42cm
+    brightness = 255 * (1 - (distance - d1)/(d2-d1)); 
+    // if the brightness ever inverts for some reason, add failsafe to make sure brightness never goes past edges, as brightness is uint8_t
+    // ex: brightness = -4 --> becomes brightness = 252
+  
     
     // control LED brightness for main LED
     OCR2A = brightness;
