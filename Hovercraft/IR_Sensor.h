64293d1e3c7c0bdf56883f81a1d516fa1b45ebd0
boileaumathieu2-droid <boileaumathieu2@gmail.com>
2026-04-08 21:16:01 -0400

    Refactor IR sensor header with new structure and functions
    Updated IRSensor structure and added function prototypes for initialization, reading, and updating sensor values.

--- Hovercraft/IR_Sensor.h
+++ Hovercraft/IR_Sensor.h
@@ -1,22 +1,29 @@
 #ifndef IR_SENSOR_H_
 #define IR_SENSOR_H_
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-// typedef struct {
-//     uint8_t IRPin;     // ADC pin for the IR sensor
-//     uint16_t reading;
-//     float distance;
-//     const uint8_t d1;  
-//     const uint8_t d2;  
-// } IRSensor;
+#define ADC_SAMPLE_MAX 4
+
+typedef struct {
+    uint8_t IRPin;     // ADC pin for the IR sensor (0-7)
+    float reading;     // averaged raw ADC reading
+    float distance;    // calculated distance in cm
+} IRSensor;
+
+// Initialize the ADC and set up the sensor pin
+void IR_sensor_initialize(IRSensor *sensor, uint8_t irpin);
+
+// Read a single ADC value from the sensor
+uint16_t IR_ADC_read(IRSensor *sensor);
+
+// Update the sensor reading and calculate distance
+void IR_sensor_update(IRSensor *sensor);
 
 #ifdef __cplusplus
 }
